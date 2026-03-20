2ce3af17bdf760e0d6d0e096df999d8382ac9893
Typolas <pierrebiason31@gmail.com>
2026-03-20 11:31:37 -0300

    aula 2 variáveis e operadores

---
+++ aula02/atividade.py
@@ -0,0 +1,4 @@
+#desafio 1
+nome = input("qual é o seu nome??")
+print(f"olá {nome}!!!")
+
---
+++ aula02/aula02a.py
@@ -0,0 +1,27 @@
+print("olá mundo")
+
+print(7+4)
+print("7+4")
+print("7" + "4") #concatenação de strings
+
+#comentário de 1 linha
+'''
+comentários de 
+mútiplas
+linhas
+'''
+
+#variáveis
+nome = "pierre"
+idade = 18
+peso = 80
+
+print(nome, idade, peso)
+print(f"oii {nome}!!!")
+
+
+#input - simulação de um forms no cmd
+nome = input("digite o seu nome: ")
+idade = int( input("digite sua idade: "))
+print(nome ,idade)
+print(idade + 99)
